67f1b853d5890031f548887fee884ed30350bd8c
Sly Elliot <contact@logicalleap.tech>
2024-12-27 23:14:25 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-import '../styles/globals.css';
+import '../styles/style.css';
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
