271e999316d8a327082cc758a713660a0a55514a
Sly Elliot <contact@logicalleap.tech>
2024-12-28 20:52:15 -0500

    vercel analytics enabled

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 // pages/_app.js
 import '../styles/globals.css'; // Ensure the correct path to styles.css
+import { Analytics } from "@vercel/analytics/react";
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
