eac149d4aa2332fc09a957ea96c65419bee33409
Sly Elliot <contact@logicalleap.tech>
2024-12-30 12:38:13 -0500

    fixing exploit

--- pages/api/save-session.js
+++ pages/api/save-session.js
@@ -50,7 +50,6 @@ export default async function handler(req, res) {
 
         return res.status(200).json({ 
             message: 'Session saved successfully',
-            userId: userId
         });
 
     } catch (error) {
